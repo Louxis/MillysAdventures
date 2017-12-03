757db16ed08318bbd63d6d87bb2aa6302769b325
Zelxy <jcpereira.dev@gmail.com>
2017-12-03 17:17:12 +0000

    Basic logic to change flower overtime

---
+++ app/src/main/java/com/milly/cm/millysadventures/FlowerLevel.java
@@ -0,0 +1,49 @@
+package com.milly.cm.millysadventures;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.SensorManager;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class FlowerLevel extends AppCompatActivity {
+    private LightTimer gameTimer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_flower_level);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        gameTimer = new LightTimer((ImageView)findViewById(R.id.flowerImage),40000,1000, (SensorManager) getSystemService(Context.SENSOR_SERVICE));
+        gameTimer.start();
+    }
+
+    @Override
+    protected void onResume(){
+        Log.d("Sensor","OnResume");
+        super.onResume();
+        gameTimer.registerSensor();
+    }
+
+    @Override
+    protected void onStop(){
+        Log.d("Sensor","OnStop");
+        gameTimer.stopSensor();
+        super.onStop();
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/milly/cm/millysadventures/LightTimer.java
@@ -0,0 +1,68 @@
+package com.milly.cm.millysadventures;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ImageView;
+
+/**
+ * Created by jcper on 12/3/2017.
+ */
+
+public class LightTimer {
+    private ImageView image;
+    private int timeleft;
+    private int timeskips;
+    private SensorManager mSensorManager;
+    private Sensor lightSensor;
+
+    public LightTimer (ImageView image, int timeleft, int timeskips, SensorManager manager){
+        this.image = image;
+        this.timeleft = timeleft;
+        this.timeskips = timeskips;
+        mSensorManager = manager;
+        lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    private final SensorEventListener mListener = new SensorEventListener(){
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.d("Light", event.values[0]/10 + "");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    public void registerSensor(){
+        mSensorManager.registerListener(mListener,lightSensor,SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    public void stopSensor(){
+        mSensorManager.unregisterListener(mListener);
+    }
+
+    public void start(){
+        new CountDownTimer(timeleft, timeskips) {
+            public void onTick(long millisUntilFinished) {
+                if(image != null){
+                    if (millisUntilFinished <= 30000 && millisUntilFinished > 20000){
+                        image.setImageResource(R.drawable.flower_stage_2);
+                    }else if(millisUntilFinished <= 20000 && millisUntilFinished > 10000){
+                        image.setImageResource(R.drawable.flower_stage_3);
+                    }else if(millisUntilFinished <= 10000){
+                        image.setImageResource(R.drawable.flower_stage_4);
+                    }
+                }
+            }
+            public void onFinish() {
+            }
+        }.start();
+    }
+}

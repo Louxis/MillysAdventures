709c930e6e5a93a7f154c6c9826359d1153e73df
Sythern <diogorocha_9@hotmail.com>
2017-12-07 22:29:25 +0000

    Reverted back to stable layout

--- app/src/main/java/com/milly/cm/millysadventures/FallingCarrots.java
+++ app/src/main/java/com/milly/cm/millysadventures/FallingCarrots.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.

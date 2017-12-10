8ccd58995e66770cd0e4e677e6fdc819ca6f1234
Diogo Rocha <diogorocha_9@hotmail.com>
2017-12-10 10:54:35 +0000

    Refactored code and added comments and documentation to methods

--- app/src/main/java/com/milly/cm/millysadventures/FallingCarrots.java
+++ app/src/main/java/com/milly/cm/millysadventures/FallingCarrots.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -127,10 +126,11 @@ public class FallingCarrots extends AppCompatActivity implements View.OnTouchLis
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        // findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        /* Upon interacting with UI controls, delay any scheduled hide()
+           operations to prevent the jarring behavior of controls going away
+           while interacting with the UI.
+         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        */
 
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -140,15 +140,29 @@ public class FallingCarrots extends AppCompatActivity implements View.OnTouchLis
         // Set touchListener on basket
         basket.setOnTouchListener(this);
 
-        // Set displaysize
+        // Set display size
         Display display = getWindowManager().getDefaultDisplay();
         displaySize = new Point();
         display.getSize(displaySize);
 
-        //Animation paramaters
-        final int amountToMoveDown = (int) Math.ceil(displaySize.y*0.75);
-        final int duration = 2000;
+        // Falling animation settings
+        int amountToMoveDown = (int) Math.ceil(displaySize.y*0.7);
+        int duration = 2000;
 
+        // Falling animation Creation
+        createFallingAnimation(amountToMoveDown, duration);
+
+        //Game Timer Start
+        timer(60000, 66);
+
+    }
+
+    /**
+     * Method that creates the pickup falling animation
+     * @param amountToMoveDown pickup movement amount based on display size
+     * @param duration animation duration - falling speed
+     */
+    private void createFallingAnimation (final int amountToMoveDown, int duration){
         anim = new TranslateAnimation(0, 0, 0, amountToMoveDown);
         anim.setDuration(duration);
         anim.setInterpolator(new LinearInterpolator() );
@@ -169,11 +183,16 @@ public class FallingCarrots extends AppCompatActivity implements View.OnTouchLis
 
             }
         });
-        timer(60000, 66);
-
     }
 
 
+    /**
+     * Method that verifies the position of the pickup, updates the level's counters,
+     * resets the pickup position and removes the pickup from the ground if the player doesn't
+     * pick it up.
+     * @param totalTime Level duration
+     * @param timeInterval Frequency of pickup detection
+     */
     private void timer(int totalTime, int timeInterval){
         pickup.startAnimation(anim);
         pickup.setContentDescription("carrot");
@@ -186,7 +205,7 @@ public class FallingCarrots extends AppCompatActivity implements View.OnTouchLis
                     if(pickup.getContentDescription().equals("carrot") ){
                         totalCarrots+=1;
                         carrotsCaugt+=1;
-                        Log.d("Carrot",totalCarrots + " Total carrots. \n" + carrotsCaugt + " Caugt");
+                        //Log.d("Carrot",totalCarrots + " Total carrots. \n" + carrotsCaugt + " Caugt");
                     }else if(pickup.getContentDescription().equals("worm") ){
                         totalWorms+=1;
                         totalWormsCaught+=1;
@@ -209,29 +228,38 @@ public class FallingCarrots extends AppCompatActivity implements View.OnTouchLis
             public void onFinish() {
             }
         }.start();
-
     }
 
+
+    /**
+     * Method that randomizes both the type of pickup and the next position that it appears
+     * on the screen and restarts the falling animation
+     */
     private void randomisePickupPosition(){
         rand = new Random();
         ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) pickup.getLayoutParams();
-        //doesn't take into account screen size
-        params.horizontalBias = (float) (rand.nextInt(9) / 10.0 + 0.1);
-        //params.verticalBias = 0;
+        params.horizontalBias = (float) (rand.nextInt(8) / 10.0 + 0.2);
         params.topMargin = 0;
-        int r = rand.nextInt(7);
-        if(r == 6){
+
+        if(rand.nextInt(7) == 6){
             pickup.setImageResource(R.drawable.wormvertical80);
             pickup.setContentDescription("worm");
         }else if(pickup.getContentDescription().equals("worm")){
             pickup.setImageResource(R.drawable.carrot80);
             pickup.setContentDescription("carrot");
         }
+
         pickup.setLayoutParams(params);
         pickup.startAnimation(anim);
     }
 
 
+    /**
+     * Method that adds onTouch interface to an object
+     * @param v Object that's going to have onTouch interface
+     * @param event Type of event that the object suffered
+     * @return
+     */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
